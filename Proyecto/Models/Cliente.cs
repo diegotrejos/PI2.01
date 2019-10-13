020fc29b0bf4efa1bfaffa2c4633badaf1bfbc0e
mariaChaves <maria.chavessalas@ucr.ac.cr>
2019-10-13 08:15:19 -0600

    Cambios en vistas

--- Proyecto/Models/Cliente.cs
+++ Proyecto/Models/Cliente.cs
@@ -11,7 +11,9 @@ namespace Proyecto.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
     public partial class Cliente
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,11 +21,21 @@ namespace Proyecto.Models
         {
             this.Proyecto = new HashSet<Proyecto>();
         }
-    
+
+        [Key]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Caracteres invalidos")]
+        [StringLength(12, MinimumLength = 9, ErrorMessage = "La cédula debe estar entre un rango de 9 a 12 digitos")]
         public string cedula { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [RegularExpression("^[a-zA-z]*$", ErrorMessage = "Caracteres invalidos")]
         public string nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [RegularExpression("^[a-zA-z]*$", ErrorMessage = "Caracteres invalidos")]
         public string apellido1 { get; set; }
+        [RegularExpression("^[a-zA-z]*$", ErrorMessage = "Caracteres invalidos")]
         public string apellido2 { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Caracteres invalidos")]
         public string telefono { get; set; }
         public string direccionExacta { get; set; }
         public string distrito { get; set; }
