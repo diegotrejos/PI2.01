2a8e784a3490ff39ac62447e14e4672d27ff435f
diegotrejos <48733767+diegotrejos@users.noreply.github.com>
2019-10-16 19:02:54 -0600

    tabla req

---
+++ Proyecto/Scripts/Distribuciones.js
@@ -0,0 +1,56 @@
+function getProvincia(){ 
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincias.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+                html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            console.log(data);
+            $("#provincia").append(html);
+        }
+    });
+}
+
+function getCanton() {
+
+    var id_provincia = $("#provincia").val();
+
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincia/"+id_provincia+"/cantones.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+                html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            console.log(data);
+            $("#canton").empty();
+            $("#canton").append(html);
+        }
+    });
+}
+
+function getDistrict() {
+
+    var id_provincia = $("#provincia").val();
+    var id_canton = $("#canton").val();
+
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincia/" + id_provincia + "/canton/" + id_canton + "/distritos.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+                html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            console.log(data);
+            $("#distrito").empty();
+            $("#distrito").append(html);
+        }
+    });
+}
---
+++ Proyecto/Scripts/SortableConnect.js
@@ -0,0 +1,22 @@
+﻿//ocupo cambiar este metodo para que se llame con un boton y en ese momento realizar los cambios 
+$(document).ready(function () {
+    $("#disponibilidad").sortable({
+        connectWith: "#asignacion",
+        update: function (event, ui) {
+            var itemIds = "";
+            $("disponibilidad").find(".taskSingleInline").each(function () {
+                var itemId = $(this).attr("data-taskid");
+                itemIds = itemIds + itemId + ",";
+            });
+            $.ajax({
+                url: '@Url.Action("UpdateItem", "Equipo")',
+                data: { itemIds: itemIds },
+                type: 'POST',
+                success: function (data) {
+                },
+                error: function (xhr, status, error) {
+                }
+            });
+        }
+    });
+})
