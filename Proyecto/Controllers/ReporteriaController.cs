80fd46b9cd27773355888f32ab512b431ce032a2
Cristy960 <cristidayan10@gmail.com>
2019-11-12 14:37:44 -0600

    index actualizado

--- Proyecto/Controllers/ReporteriaController.cs
+++ Proyecto/Controllers/ReporteriaController.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto.Models;
+using System.Data.SqlClient;
+using System.Windows;
 
 namespace Proyecto.Controllers
 {
     public class ReporteriaController : Controller
     {
+        public string usuario = "";
+        public string cedula = "";
+        public string proy = "";
+
+        private Gr02Proy3Entities db = new Gr02Proy3Entities();
+        Proyecto.Controllers.ProyectoController proyController = new Proyecto.Controllers.ProyectoController();
         // GET: Reporteria
         public ActionResult Index()
         {
+            string usuario = System.Web.HttpContext.Current.Session["rol"] as string;
+            string proy = System.Web.HttpContext.Current.Session["proyecto"] as string;
+            string cedula = System.Web.HttpContext.Current.Session["cedula"] as string;
             return View();
         }
 
