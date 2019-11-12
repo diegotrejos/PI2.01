588101d5bc7e0e6df2f01e136c26ae1d43aefcdd
Cristy960 <cristidayan10@gmail.com>
2019-11-12 14:30:42 -0600

    Agregada visra reporteria

---
+++ Proyecto/Controllers/ReporteriaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class ReporteriaController : Controller
+    {
+        // GET: Reporteria
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
