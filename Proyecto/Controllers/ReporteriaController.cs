31f1bdba6c696b0be5d5cecb1f052df024560729
mariaChaves <maria.chavessalas@ucr.ac.cr>
2019-11-20 14:38:24 -0600

    Agregadas las dos vistas de las consultas

--- Proyecto/Controllers/ReporteriaController.cs
+++ Proyecto/Controllers/ReporteriaController.cs
@@ -26,9 +26,37 @@ namespace Proyecto.Controllers
             string usuario = System.Web.HttpContext.Current.Session["rol"] as string;
             string proy = System.Web.HttpContext.Current.Session["proyecto"] as string;
             string cedula = System.Web.HttpContext.Current.Session["cedula"] as string;
+            List<Proyecto.Models.Proyecto> proyectos = new ProyectoController().gettProyectos(usuario, cedula);
+           
+            ViewBag.cedulaEmpleadoPK_FK = new SelectList(db.EmpleadoDesarrollador, "cedulaED", "nombreED");
+            ViewBag.user = usuario;
+            TempData["Complejidad"] = crearListaComplejidad();
+           
+            TempData.Keep();
             return View();
         }
 
-      
+
+        public ActionResult ComparacionComplejidad()
+        {
+            return View();
+        }
+
+        public ActionResult HistorialDesarrollador()
+        {
+            ViewBag.cedulaEmpleadoPK_FK = new SelectList(db.EmpleadoDesarrollador, "cedulaED", "nombreED");
+            return View();
+        }
+
+        //Crea la lista predefinida con las complejidades
+        private List<string> crearListaComplejidad()
+        {
+            List<string> listaLocal = new List<string>();
+            listaLocal.Add("Simple");
+            listaLocal.Add("Mediano");
+            listaLocal.Add("Complejo");
+            listaLocal.Add("Muy Complejo");
+            return listaLocal;
+        }
     }
 }
