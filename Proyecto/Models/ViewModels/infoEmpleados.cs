c9150af178a2ca480afc3218b06817442ee73771
TheCcromer <33296759+TheCcromer@users.noreply.github.com>
2019-12-01 18:00:29 -0600

    Clases para las consultas #1 y #13

---
+++ Proyecto/Models/ViewModels/infoEmpleados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models.ViewModels
+{
+    public class infoEmpleados
+    {
+        public Equipo equipo { get; set; }
+        public List<Requerimiento> requerimientos { get; set; }
+        public DateTime durEstimada { get; set; }
+        public int cantidadReq { get; set; }
+    }
+}
