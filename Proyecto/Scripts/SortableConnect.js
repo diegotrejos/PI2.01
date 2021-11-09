99dff38222ebcd143ba9b573ab7517804421f7d3
diegotrejos <48733767+diegotrejos@users.noreply.github.com>
2021-11-08 23:20:01 -0600

    elim scripts

--- Proyecto/Scripts/SortableConnect.js
+++
@@ -1,6 +0,0 @@
-﻿//ocupo cambiar este metodo para que se llame con un boton y en ese momento realizar los cambios 
-$(document).ready(function () {
-    $("#disponibilidad").sortable({
-        connectWith: "#asignacion"
-    })
-});
